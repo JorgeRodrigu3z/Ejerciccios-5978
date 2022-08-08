e1efe36b4c4ca49533c19fdc61657017ed260078
Jorge Rodriguez <jr4436531@gmail.com>
2022-08-08 09:05:00 -0500

    1er commit v1 cuerpo inicial del archivo

---
+++ ordena_letras.cpp
@@ -0,0 +1,54 @@
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string.h>
+using namespace std;
+
+void llenaletras(char p[])
+{
+    int lc, i;
+    srand(time(NULL));
+    lc = 1 + rand() % 20;
+    for (i = 0; i < lc; i++)
+    {
+        p[i] = 97 + rand() % 26;
+    }
+    p[i] = '\0';
+
+    // a-z 97-122 o A-Z 65 - 90
+}
+
+
+void ordenaletras(char p[])
+{
+    char aux;
+    int i, j;
+    for (i = 0; i < strlen(p) - 1; i++)
+    {
+        for (j = i + 1; j < strlen(p); j++)
+        {
+            if (p[i] > p[j])
+            {
+                aux = p[i];
+                p[i] = p[j];
+                p[j] = aux;
+            }
+        }
+    }
+}
+
+void buscar_letras(char v[])
+{
+
+}
+
+
+main()
+{
+    char letras[100];
+    llenaletras(letras);
+    cout << letras << "\n";
+    ordenaletras(letras);
+    cout << letras;
+}
